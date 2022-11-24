fed0887fd9067077d1be51e3c5670c1580b73e34
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-11-24 15:16:04 +0530

    learned a creating a server and sending dummy data

--- readme.md
+++ readme.md
@@ -6,3 +6,9 @@ npm init or npm init -y
 npm i -D nodemon
 
 ## -D for dev dependency only for development not for production
+
+## create server.js file
+
+and configure the package.json
+    "start": "node server.js",
+    "dev": "nodemon server.js",
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+// console.log('hello world');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello world</h1>');
+    res.end();
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server is running on the port ${PORT}`))
