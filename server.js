c2ef4f7e15df32ddca48857bd315a76910d317dd
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-11-24 15:29:28 +0530

    feat: send json without routing

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // console.log('hello world');
-
+const products = require('./data/products.json');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello world</h1>');
-    res.end();
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1>Hello world</h1>');
+    // res.end();
+    // res.writeHead(200, { 'Content-Type': 'application/json'});
+    // res.end(JSON.stringify(products));
 });
 
 const PORT = process.env.PORT || 5000;
