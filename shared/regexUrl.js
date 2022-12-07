6c5ef9dbdefc725ad2f0640a2ba169f4b8326170
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-12-07 17:08:36 +0530

    fix: params id regex

--- shared/regexUrl.js
+++ shared/regexUrl.js
@@ -1,5 +1,6 @@
 // const getProdRegex = /\/api\/product\/([0-9]+)/;
-const getProdRegex = /\/api\/product\/([A-z]*[0-9]*[-]*)/
+// const getProdRegex = /\/api\/product\/([A-z]*[0-9]*[-]*)/;
+const getProdRegex = /\/api\/product\/([\d\w -]*)/;
 module.exports = {
     getProdRegex
 }
